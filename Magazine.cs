558cb07fe43043c3abcc4a7cecc1e82cbc86842f
Moldir <51477205+Moldir99@users.noreply.github.com>
2022-06-02 00:20:45 +0300

    Update Magazine.cs

--- Magazine.cs
+++ Magazine.cs
@@ -1,3 +1,6 @@
+// This script is responsible for the number of bullets in the magazine. 
+// It is given number 8, but it gives possibility in the environment to set up any number.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
